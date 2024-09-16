3b1a50044dc7cb4dbe96c2d3efd35e19a415d561
Shaghayegh Beheshti Pouya <adonia.works@gamil.com>
2024-09-16 16:06:08 +0330

    Make a new components for a duplicated  styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,8 +11,7 @@ interface Props {
 
 const GameCard = ({game} : Props) => {
   return (
-    //note to myself: remove this width='300px' for better view 
-<Card width='300px' borderRadius={10} overflow='hidden'>
+<Card>
     <Image src={getCroppedImageUrl(game.background_image)}/>
     <CardBody>
         <Heading fontSize='2xl' >{game.name}</Heading>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid, Skeleton, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
+import GameCardContainer from './GameCardContainer';
 
 
 const GameGrid = () => {
@@ -12,9 +13,14 @@ const GameGrid = () => {
    <>
    {error && <Text>{error}</Text>}
     <SimpleGrid columns={{sm:1, md:2, lg:3, xl:5}} padding='10px' spacing={10}>
-      {isLoading && skeletons.map(skeleton=> <GameCardSkeleton key={skeleton}/>)}
+      {isLoading && skeletons.map(skeleton=><GameCardContainer>
+        <GameCardSkeleton key={skeleton}/>
+      </GameCardContainer> )}
         {games.map((game )=>(
+         // Note: I dont like this styles
+         // <GameCardContainer>
           <GameCard key={game.id} game={game}/>
+          //</GameCardContainer>
       ))}
     </SimpleGrid>
    </>
